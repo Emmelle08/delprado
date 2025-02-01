54c606775fc1e6d9747605d65573a09d72b45374
root <root@DESKTOP-UCCKA32.>
2025-02-01 13:10:13 +0800

    added syntax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+import { HeaderComponent } from './post/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
